6d49eaea7cce89c6c0e94f132434a106653b110d
CORNEC <josquin.cornec@insa-lyon.fr>
2019-01-18 10:57:21 +0100

    New benchmark script using perf timechart

--- simulation_example_1/doBenchmark.sh
+++ simulation_example_1/doBenchmark.sh
@@ -1,3 +1,15 @@
-sudo perf timechart record ../pdc_mini_aevol -n 50 &&
-sudo perf timechart &&
-gwenview output.svg & 
+#!/bin/bash
+rouge='\e[0;31m'
+neutre='\e[0;m'
+
+/bin/bash -c "cd .. ; make" &&
+sudo perf timechart record ../pdc_mini_aevol -n ${1} &&
+sudo perf timechart -P &&
+gwenview output.svg &
+
+#On vérifie que les résultats sont corrects
+if ["" -eq $(diff stats/stats_simd_mean.csv stats/stats_simd_mean2.csv)]; then 
+	echo "Le résultat est correct"; 
+else 
+	echo -e "${rouge}RESULTAT INCORRECT${neutre}";
+fi
