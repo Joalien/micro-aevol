cb0c37c4197e285c40de87762d8bc5e13563f866
CORNEC <josquin.cornec@insa-lyon.fr>
2019-01-25 11:11:10 +0100

    Improvement of chart in benchmark

--- simulation_example_1/doBenchmark.sh
+++ simulation_example_1/doBenchmark.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 rouge='\e[0;31m'
 neutre='\e[0;m'
+vert='\e[1;32m'
 
 /bin/bash -c "cd .. ; make" &&
 sudo perf timechart record ../pdc_mini_aevol -n ${1} &&
-sudo perf timechart -P &&
-gwenview output.svg &
+sudo perf timechart -p 'pdc_mini_aevol' --highlight 'pdc_mini_aevol' &&
+gwenview output.svg 
 
 #On vérifie que les résultats sont corrects
-if ["" -eq $(diff stats/stats_simd_mean.csv stats/stats_simd_mean2.csv)]; then 
-	echo "Le résultat est correct"; 
+if [ "0" -eq $(diff stats/stats_simd_mean.csv stats/stats_simd_mean2.csv | wc -w) ]; then 
+	echo -e "${vert}RESULTAT CORRECT${neutre}"; 
 else 
 	echo -e "${rouge}RESULTAT INCORRECT${neutre}";
 fi
