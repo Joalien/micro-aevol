151df87b1bdd9e8d8326cceb2a32c0ee7715815a
CORNEC <josquin.cornec@insa-lyon.fr>
2019-02-08 00:52:08 +0100

    Force timer to be prefixed with TIME: in order to parse

--- ExpManager.cpp
+++ ExpManager.cpp
@@ -112,8 +112,6 @@ ExpManager::ExpManager(int grid_height, int grid_width, int seed, double mutatio
         geometric_area_+=((fabs(target[i]) + fabs(target[i+1])) / (600.0));
     }
 
-    printf("Initialized environmental target %f\n",geometric_area_);
-
 
     // Initializing the PRNGs
     for (int indiv_id = 0; indiv_id < nb_indivs_; ++indiv_id) {
@@ -143,7 +141,6 @@ ExpManager::ExpManager(int grid_height, int grid_width, int seed, double mutatio
 
     }
 
-    printf("Populating the environment\n");
 
     // Create a population of clones based on the randomly generated organism
     for (int indiv_id = 0; indiv_id < nb_indivs_; indiv_id++) {
@@ -1414,6 +1411,10 @@ void ExpManager::run_evolution(int nb_gen) {
             //save(AeTime::time());
         }
     }
+
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    auto duration_gpu_start_stop_rna = std::chrono::duration_cast<std::chrono::milliseconds>( t2 - t1 ).count();
+    printf("TIME:%d\n",duration_gpu_start_stop_rna);
 }
 
 
